691b5ee52375cfafe913e23320d9ab6487efdec5
Santhosh <136286484+santhoshmayilraj@users.noreply.github.com>
2025-03-30 10:44:40 +0530

    Time: 8 ms (78.26%) | Memory: 51.9 MB (83.60%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,54 +1,42 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        vector<int> visited(graph.size(),0);
         vector<int> ans;
-        unordered_map<int,int> indegree;
-        vector<vector<int>> indegree_graph(graph.size());
         for(int i=0;i<graph.size();i++)
         {
-            indegree[i] = graph[i].size();
-            for(int j:graph[i])
+            if(visited[i]==0)
             {
-                indegree_graph[j].push_back(i);
+                helper(visited,ans,i,graph);
             }
-        }
-        queue<int> temp;
-        for(int i=0;i<indegree.size();i++)
-        {
-            if(indegree[i]==0)
-            {
-                temp.push(i);
-                ans.push_back(i);
-            }
-        }
-        int prev = -1;
-        int sz = ans.size();
-        while(!temp.empty())
-        {
-            if(prev == sz)
-            {
-                sort(ans.begin(),ans.end());
-                return ans;
-            }
-            int curr_sz = temp.size();
-            for(int i=0;i<curr_sz;i++)
-            {
-                int curr = temp.front();
-                temp.pop();
-                for(int k:indegree_graph[curr])
-                {
-                    indegree[k]--;
-                    if(indegree[k]==0)
-                    {
-                        ans.push_back(k);
-                        temp.push(k);
-                    }
-                }        
-            }
-            prev = sz;
-            sz = ans.size();
-        }
-        sort(ans.begin(),ans.end());
+        }   
+        sort(ans.begin(),ans.end());     
         return ans;
+    }
+
+    void helper(vector<int> &visited,vector<int> &ans,int curr,vector<vector<int>> &graph)
+    {
+        if(visited[curr]!=0)
+        {
+            return;
+        }
+        visited[curr] = 1;
+        for(int i:graph[curr])
+        {
+            if(visited[i] == 1)
+            {
+                return;
+            }
+            if(visited[i] == 0)
+            {
+                helper(visited,ans,i,graph);
+                if(visited[i] == 1)
+                {
+                    return;
+                }
+            }
+        }
+        visited[curr] = 2;
+        ans.push_back(curr);
     }
 };
