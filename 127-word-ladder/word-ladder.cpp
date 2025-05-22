c7872d621481b339cc3e651ba95996037eb81d40
Santhosh <136286484+santhoshmayilraj@users.noreply.github.com>
2025-05-22 09:47:53 +0530

    Time: 54 ms (78.73%) | Memory: 19.6 MB (94.16%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,50 +1,42 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_map<string,int> hash;
-        if(beginWord == endWord)
-        {
+        unordered_set<string> wordSet(wordList.begin(), wordList.end());
+        if (wordSet.find(endWord) == wordSet.end()) {
             return 0;
         }
-        for(string i:wordList)
-        {
-            hash[i] = 1;
-        }
+
         queue<string> q;
         q.push(beginWord);
-        int ans = 1;
-        vector<char> alpha;
-        for (char ch = 'a'; ch <= 'z'; ++ch)
-        {
-            alpha.push_back(ch);
-        }
-        while(!q.empty())
-        {
-            ans++;
+        int steps = 1;
+
+        while (!q.empty()) {
             int sz = q.size();
-            for(int i=0;i<sz;i++)
-            {
+            for (int i = 0; i < sz; ++i) {
                 string curr = q.front();
                 q.pop();
-                for(int i=0;i<curr.size();i++)
-                {
-                    string curr1 = curr;
-                    for(char ch:alpha)
-                    {
-                        curr1[i] = ch;
-                        if(hash[curr1] == 1)
-                        {
-                            if(curr1 == endWord)
-                            {
-                                return ans;
-                            }
-                            hash[curr1] = 2;
-                            q.push(curr1);
+
+                for (int j = 0; j < curr.size(); ++j) {
+                    char original = curr[j];
+                    for (char ch = 'a'; ch <= 'z'; ++ch) {
+                        if (ch == original) continue;
+                        curr[j] = ch;
+
+                        if (curr == endWord) {
+                            return steps + 1;
+                        }
+
+                        if (wordSet.find(curr) != wordSet.end()) {
+                            q.push(curr);
+                            wordSet.erase(curr);
                         }
                     }
+                    curr[j] = original;  
                 }
             }
+            steps++;
         }
+
         return 0;
     }
 };
