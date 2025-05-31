01bccc93754cc2fe5e2f33f4827e3496be756df7
Santhosh <136286484+santhoshmayilraj@users.noreply.github.com>
2025-05-31 15:48:51 +0530

    Time: 152 ms (5.27%) | Memory: 54.2 MB (5.01%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,34 +1,24 @@
 class Solution {
+    // solving the problem using Bellman - Ford Algorithm 
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> adj(n);
-        for (const auto& flight : flights) {
-            adj[flight[0]].emplace_back(flight[1], flight[2]);
-        }
-
-        // Priority queue: {cost, current_node, stops}
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
-        pq.emplace(0, src, 0);
-
-        // We store visited[node][stops] = min cost
-        vector<vector<int>> visited(n, vector<int>(k + 2, INT_MAX));
-        visited[src][0] = 0;
-
-        while (!pq.empty()) {
-            auto [cost, node, stops] = pq.top(); pq.pop();
-
-            if (node == dst) return cost;
-            if (stops > k) continue;
-
-            for (auto& [nei, price] : adj[node]) {
-                int new_cost = cost + price;
-                if (new_cost < visited[nei][stops + 1]) {
-                    visited[nei][stops + 1] = new_cost;
-                    pq.emplace(new_cost, nei, stops + 1);
+        vector<int> distance(n,INT_MAX);
+        distance[src] = 0;
+        for(int i=0;i<k+1;i++)
+        {
+            vector<int> temp = distance;
+            for(vector<int> j:flights)
+            {
+                int u = j[0];
+                int v = j[1];
+                int w = j[2];
+                if(distance[u] != INT_MAX and temp[v] > distance[u] + w)
+                {
+                    temp[v] = distance[u] + w;
                 }
             }
+            distance = temp;
         }
-
-        return -1;
+        return distance[dst] == INT_MAX ? -1:distance[dst];
     }
 };
