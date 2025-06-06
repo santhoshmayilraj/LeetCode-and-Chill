18944843797b56b46ea77c6401fafc8299053f15
Santhosh <136286484+santhoshmayilraj@users.noreply.github.com>
2025-06-06 19:27:01 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (5.78%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,28 +1,48 @@
-class Solution { // this is the bottom-up approach 
+class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        vector<vector<int>> dp = triangle;
-        for(int i=0;i<dp.size();i++)
-        {
-            for(int j=0;j<dp[i].size();j++)
-            {
-                dp[i][j] = INT_MAX;
+        vector<vector<int>> temp = triangle;
+        for (int i = 0; i < temp.size(); ++i) {
+            for (int j = 0; j < temp[i].size(); ++j) {
+                temp[i][j] = INT_MAX;
             }
         }
-        dp[0][0] = triangle[0][0];
-        for(int i=0;i<dp.size()-1;i++)
-        {
-            for(int j=0;j<dp[i].size();j++)
-            {
-                dp[i+1][j] = min(dp[i+1][j],dp[i][j]+triangle[i+1][j]);
-                dp[i+1][j+1] = min(dp[i+1][j+1],dp[i][j]+triangle[i+1][j+1]);
-            }
+
+        int min_value = INT_MAX;
+        int last_row = temp.size() - 1;
+        for (int j = 0; j < temp[last_row].size(); ++j) {
+            min_value = min(min_value, helper(triangle, temp, last_row, j));
         }
-        int min_ans = INT_MAX;
-        for(int i=0;i<dp[dp.size()-1].size();i++)
-        {
-            min_ans = min(min_ans,dp[dp.size()-1][i]);
+        return min_value;
+    }
+
+private:
+    int helper(vector<vector<int>>& triangle, vector<vector<int>>& temp, int curr_x, int curr_y) {
+        // Boundary check
+        if (curr_x < 0 || curr_y < 0 || curr_y >= triangle[curr_x].size()) {
+            return INT_MAX;
         }
-        return min_ans;
+
+        // Base case: top of the triangle
+        if (curr_x == 0 && curr_y == 0) {
+            return triangle[0][0];
+        }
+
+        // Return already computed value
+        if (temp[curr_x][curr_y] != INT_MAX) {
+            return temp[curr_x][curr_y];
+        }
+
+        // Recursively compute the minimum path sum to current cell
+        int from_left = helper(triangle, temp, curr_x - 1, curr_y - 1);
+        int from_above = helper(triangle, temp, curr_x - 1, curr_y);
+
+        int min_prev = min(from_left, from_above);
+        if (min_prev == INT_MAX) {
+            return INT_MAX;
+        }
+
+        temp[curr_x][curr_y] = min_prev + triangle[curr_x][curr_y];
+        return temp[curr_x][curr_y];
     }
 };
