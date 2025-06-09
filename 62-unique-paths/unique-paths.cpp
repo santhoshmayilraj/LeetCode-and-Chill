a1a6c5f448d0ff552eef631453ebf8a8e6c3173d
Santhosh <136286484+santhoshmayilraj@users.noreply.github.com>
2025-06-09 11:27:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (71.75%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,22 @@
+class Solution { // bottom-up space optimization 
+public:
+    int uniquePaths(int m, int n) {
+        int mod = 2*(pow(10,9));
+        if(m==0 and n==0)
+        {
+            return 0;
+        }
+        vector<int> dp(n,1);
+        for(int i=1;i<m;i++)
+        {
+            vector<int> curr(n,0);
+            curr[0] = 1;
+            for(int j=1;j<n;j++)
+            {
+                curr[j] = curr[j-1] + dp[j];
+            }
+            dp = curr;
+        }
+        return dp[n-1];
+    }
+};
