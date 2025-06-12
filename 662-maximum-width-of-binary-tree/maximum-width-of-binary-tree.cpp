18d741c91b57d9e030f689cbe5ff465bce0d2a9d
Santhosh <136286484+santhoshmayilraj@users.noreply.github.com>
2025-06-12 20:05:43 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (10.44%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -1,45 +1,35 @@
-						// \U0001f609\U0001f609\U0001f609\U0001f609Please upvote if it helps \U0001f609\U0001f609\U0001f609\U0001f609
+#include <climits>
+#include <queue>
+
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if(root == NULL)
-            return 0;
-        
-        long long res = 1;
+        long long ans = 0;
         queue<pair<TreeNode*, long long>> q;
+        q.push({root, 0});
         
-        // I am using intialising list
-        q.push({root, 0});      // also can use make_pair
-        
-        while(!q.empty())
-        {
-            long long cnt = q.size();
-            // start is the index of root node for first level
-            long long start = q.front().second;
-            long long end = q.back().second;
+        while (!q.empty()) {
+            int sz = q.size();
+            long long first_val = q.front().second;
+            long long curr_min = LLONG_MAX, curr_max = 0;
             
-            res = max(res,end-start + 1);
-            
-            for(int i = 0; i <cnt; ++i)
-            {
-                pair<TreeNode*, int> p = q.front();
-                // we will use it while inserting it children
-                // left child will be 2 * idx + 1;
-                // right chils will be 2 * idx + 2;
-                long long idx = p.second - start;
+            for (int i = 0; i < sz; ++i) {
+                auto curr = q.front(); q.pop();
+                TreeNode* curr_node = curr.first;
+                long long curr_value = curr.second - first_val;
                 
-                q.pop();
+                curr_min = min(curr_min, curr_value);
+                curr_max = max(curr_max, curr_value);
                 
-                // if  left child exist
-                if(p.first->left != NULL)
-                    q.push({p.first->left, (long long)2 * idx + 1});
-                
-                if(p.first->right != NULL)
-                    q.push({p.first->right, (long long) 2 * idx + 2});
+                if (curr_node->left)
+                    q.push({curr_node->left, curr_value * 2});
+                if (curr_node->right)
+                    q.push({curr_node->right, curr_value * 2 + 1});
             }
+            
+            ans = max(ans, curr_max - curr_min + 1);
         }
         
-        return res;
-        
+        return static_cast<int>(ans);
     }
 };
