5c684c773fd8d8c6cfba528428b4ca03a2b1849c
Santhosh <136286484+santhoshmayilraj@users.noreply.github.com>
2025-07-30 09:23:38 +0530

    Time: 644 ms (5.06%) | Memory: 0B (100.00%) - LeetSync

---
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -0,0 +1,6 @@
+-- Write your PostgreSQL query statement below
+SELECT DISTINCT(t1.num) AS ConsecutiveNums
+FROM Logs t1
+WHERE t1.id IN (SELECT t2.id FROM Logs t2 WHERE t1.num = t2.num )
+AND t1.id-1 IN (SELECT t2.id FROM Logs t2 WHERE t1.num = t2.num )
+AND t1.id+1 IN (SELECT t2.id FROM Logs t2 WHERE t1.num = t2.num );
