40d5734fdb6cd1be1adcbcf9b43df2b13a3b87c7
Santhosh <136286484+santhoshmayilraj@users.noreply.github.com>
2025-08-23 11:02:44 +0530

    Time: 4 ms (75.83%) | Memory: 30.3 MB (37.82%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -2,17 +2,30 @@ class Solution {
 public:
     int countSquares(vector<vector<int>>& matrix) {
         int ans = 0;
-        vector<vector<int>> dp(matrix.size(),vector<int>(matrix[0].size(),-1));
-        for(int i=0;i<matrix.size();i++)
+        vector<vector<int>> dp(matrix.size()+1,vector<int>(matrix[0].size()+1,0));
+
+        for(int i=matrix.size()-1;i>=0;i--)
         {
-            for(int j=0;j<matrix[0].size();j++)
+            for(int j=matrix[0].size()-1;j>=0;j--)
             {
                 if(matrix[i][j] == 1)
                 {
-                    ans += helper(i,j,matrix,dp);
+                    dp[i][j] = min({dp[i+1][j],dp[i][j+1],dp[i+1][j+1]}) + 1;
+                    ans+= dp[i][j];
                 }
             }
         }
+
+        // for(int i=0;i<matrix.size();i++)
+        // {
+        //     for(int j=0;j<matrix[0].size();j++)
+        //     {
+        //         if(matrix[i][j] == 1)
+        //         {
+        //             ans += helper(i,j,matrix,dp);
+        //         }
+        //     }
+        // }
         return ans;
     }
 
