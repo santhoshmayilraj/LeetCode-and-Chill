9f686f49cb04e00e4e7e870d9aaa3a29b32c1d5a
Santhosh <136286484+santhoshmayilraj@users.noreply.github.com>
2025-08-23 09:39:23 +0530

    Time: 8 ms (47.73%) | Memory: 30.4 MB (37.82%) - LeetSync

---
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        int ans = 0;
+        vector<vector<int>> dp(matrix.size(),vector<int>(matrix[0].size(),-1));
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                if(matrix[i][j] == 1)
+                {
+                    ans += helper(i,j,matrix,dp);
+                }
+            }
+        }
+        return ans;
+    }
+
+    int helper(int x,int y,vector<vector<int>> &matrix,vector<vector<int>> &dp)
+    {
+        int temp = 1;
+        int curr = 0;
+        if(dp[x][y] != -1)
+        {
+            return dp[x][y];
+        }
+        if(x+1 < matrix.size() and matrix[x+1][y] == 1)
+        {
+            curr = helper(x+1,y,matrix,dp);
+        }
+        if(y+1 < matrix[0].size() and matrix[x][y+1] == 1)
+        {
+            curr = min(curr,helper(x,y+1,matrix,dp));
+        }
+        else
+        {
+            curr = 0;
+        }
+        if(x+1 < matrix.size() and y+1 < matrix[0].size() and matrix[x+1][y+1] == 1)
+        {
+            curr = min(curr,helper(x+1,y+1,matrix,dp));
+        }
+        else
+        {
+            curr = 0;
+        }
+        dp[x][y] = temp+curr;
+        return temp+curr;
+    }
+};
